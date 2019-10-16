9f051f3a44a5e542e615d401232ce08407d9c2e1
martin donadieu <martindonadieu@gmail.com>
2019-10-16 17:16:21 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:riderx/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:riderx/indiemakerfr-landing-page.git master:gh-pages
+git push -f git@github.com:riderx/indiemakerfr.git master:gh-pages
 
 cd -
