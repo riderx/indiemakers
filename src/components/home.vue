b7a00ef29dfbca3f16bf071059b964107c5447cc
martin donadieu <martindonadieu@gmail.com>
2019-10-31 10:45:45 +0000

    fix others projects

--- src/components/home.vue
+++ src/components/home.vue
@@ -20,7 +20,7 @@
       <div class="col-12 col-md-6 pt-0 px-md-5 order-1 order-md-2">
         <IllustrationHome />
       </div>
-      <div class="col-12 pt-0 px-md-5 order-1 order-md-2 text-white">
+      <div class="col-12 pt-0 px-md-5 order-3 text-white">
         <h5>Mes autres projets:</h5>
         <a
           class="text-white"
