d181e78d27292ea9112f5d4c339912e54ef83d8c
BobCashStory <bob@cashstory.com>
2020-07-05 19:59:51 +0200

    use ssr vmodal

--- plugins/global.client.js
+++ plugins/global.client.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import 'vue-directive-tooltip/dist/vueDirectiveTooltip.css'
-import VModal from 'vue-js-modal'
+import VModal from 'vue-js-modal/dist/ssr.index'
 import 'vue-js-modal/dist/styles.css'
 import VuePlyr from 'vue-plyr'
 import Tooltip from 'vue-directive-tooltip'
