0f85feef4afff82bcd5caba8636625aeb8062ae8
riderx <martindonadieu@gmail.com>
2020-07-20 02:21:39 +0200

    remove useless logs

--- plugins/rss.js
+++ plugins/rss.js
@@ -3,10 +3,8 @@ const parser = new Parser()
 
 export const feed = () => {
   if (process.env.dev) {
-    console.log('dev', `${process.env.domain_local}/${process.env.baseRSS}`)
     return parser.parseURL(`${process.env.domain_local}/${process.env.baseRSS}`)
   } else {
-    console.log('prod', `${process.env.domain_local}/${process.env.baseRSS}`)
     return parser.parseURL(`${process.env.domain}/${process.env.baseRSS}`)
   }
 }
