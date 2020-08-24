68dd38b8395067c1cfecc27e05a073d3b3c41ca8
riderx <martindonadieu@gmail.com>
2020-08-24 11:03:04 +0200

    fix for singlesend

--- functions/src/email.ts
+++ functions/src/email.ts
@@ -5,7 +5,8 @@ const sgListIndie = '9d1a4cc5-92bf-4253-a408-b3277fc36ea4'
 const sgListEp = 'dbedce13-afa8-48f6-8149-030a8505f50e'
 
 const sgEpdDesignId = '1362115d-6d78-437f-a8a6-c6c4921e5fee'
-const suppressionGroupId = '28378'
+const suppressionGroupId = 28378
+const senderId = 950604
 
 export const imageLink = 'http://cdn.mcauto-images-production.sendgrid.net/d0d537150a8a5f88/40072b22-6c48-407f-9dd6-bc16d3769803/512x512.png'
 export const imageStyle = 'display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:50% !important; width:50%; height:auto !important;'
@@ -67,26 +68,64 @@ export const UpdateDesign = async (designId: string, html_content: string, subje
   }
 }
 
-export const sendEpToContactList = async (title: string, designId: string) => {
+export const getSingleSend = async (id: string) => {
+  const request: ClientRequest = {
+    method: 'GET',
+    url: `/v3/marketing/singlesends/${id}`,
+    // url: `/v3/marketing/singlesends`,
+  }
+  try {
+    return await sgClient.request(request)
+  } catch (err) {
+    console.error('getSingleSend err', err)
+    console.error('getSingleSend err', err.response.body.errors)
+    return null
+  }
+}
+
+export const createSingleSend = async (name: string, designId: string) => {
+  const send_at = new Date(Date.now() + 60000);
   const request: ClientRequest = {
     method: 'POST',
     url: '/v3/marketing/singlesends',
     body: {
-      name: title,
+      send_at,
+      name,
       send_to: {
         list_ids: [sgListEp]
       },
       email_config: {
-        subject: title,
+        sender_id: senderId,
         design_id: designId,
         suppression_group_id: suppressionGroupId
       }
     }
   }
   try {
-    await sgClient.request(request)
+    const [res, singlesend] =  await sgClient.request(request)
+    console.log('res', res.statusCode);
+    return singlesend.id;
+  } catch (err) {
+    console.error('createSingleSend err', err)
+    console.error('createSingleSend err', err.response.body.errors)
+    return null
+  }
+}
+
+export const sendEpToContactList = async (id: string) => {
+  const request: ClientRequest = {
+    method: 'PUT',
+    url: `/v3/marketing/singlesends/${id}/schedule`,
+    body: {
+      send_at: 'now',
+    }
+  }
+  try {
+    return await sgClient.request(request)
   } catch (err) {
     console.error('sendEpToContactList err', err)
+    console.error('sendEpToContactList err', err.response.body.errors)
+    return null
   }
 }
 
@@ -109,7 +148,8 @@ export const sendEmailEp = async (episode: Episode) => {
     htmlContent = htmlContent.replace(imageStyle, `${imageStyle}border-style: solid;border-width: 10px;border-color: white;`)
     htmlContent = htmlContent.replace(imageLink, image)
     await UpdateDesign(designId, htmlContent, preview)
-    await sendEpToContactList(title, designId)
+    const sendId = await createSingleSend(title, designId)
+    await sendEpToContactList(sendId)
   }
 }
 
