b1fd10044e5b9fd99424ded1c0c02fdde922ff44
riderx <martindonadieu@gmail.com>
2020-10-19 05:29:29 +0200

    fix: playr set currentitme

--- pages/episode/_id.vue
+++ pages/episode/_id.vue
@@ -232,7 +232,7 @@ export default {
             localStorage.setItem(this.$route.params.id, this.player2.currentTime)
           })
         }
-      }, 150)
+      }, 250)
     }, 2000)
     this.timeoutModal = setTimeout(() => {
       this.showRandomModal()
