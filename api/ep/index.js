9d827047c93d6ab6d41049b5131a970a52328954
riderx <martindonadieu@gmail.com>
2020-12-16 10:07:04 +0000

    fix: import order

--- api/ep/index.js
+++ api/ep/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/first */
+global.fetch = require('node-fetch')
+
 import { Database } from 'firebase-firestore-lite'
 
 const util = require('../../plugins/feed')
-global.fetch = require('node-fetch')
 
 const projectId = 'indiemakerfr'
 // Now pass the auth instance as well as the projectId.
