c2709c98a80df8bf429d5bda05677a3cc9ac98a0
riderx <martindonadieu@gmail.com>
2021-05-01 01:53:05 +0100

    fix: colors in articles

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,9 +1,6 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
-  <div class="relative px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-    <div class="absolute inset-0">
-      <div class="bg-white h-1/3 sm:h-2/3" />
-    </div>
+  <div class="relative px-4 pt-16 pb-20 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
     <div class="relative mx-auto max-w-7xl">
       <div class="text-center">
         <h2 class="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
@@ -16,7 +13,7 @@
       <div class="grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none">
         <client-only>
           <NuxtLink v-for="article in articles" :key="article.slug" :to="`/articles/${article.slug}`" class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-            <div class="flex-shrink-0 h-48">
+            <div class="flex-shrink-0 h-48 bg-gray-600">
               <img
                 class="object-cover w-full h-48"
                 width="100%"
@@ -25,7 +22,7 @@
                 :alt="article.title"
               >
             </div>
-            <div class="flex flex-col justify-between flex-1 p-6">
+            <div class="flex flex-col justify-between flex-1 p-6 bg-white">
               <div class="flex-1">
                 <p class="text-sm font-medium text-indigo-600">
                   <a href="#" class="hover:underline">
