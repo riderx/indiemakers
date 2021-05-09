262ae100a8d535c47272669c8bc5049dc705d9d2
riderx <martindonadieu@gmail.com>
2021-05-09 03:08:47 +0100

    fix: remove login form and redirect to community

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -80,8 +80,8 @@
           </div>
         </nav>
         <div class="flex items-center md:ml-12">
-          <a href="/login" class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-orchid-300 hover:bg-orchid-600">
-            Se connecter
+          <a href="/discord" class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-orchid-300 hover:bg-orchid-600">
+            Communauté
           </a>
         </div>
       </div>
@@ -148,8 +148,8 @@
             </a>
           </div>
           <div class="mt-6">
-            <a href="/login" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-orchid-300 hover:bg-orchid-600">
-              Se connecter
+            <a href="/discord" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-orchid-300 hover:bg-orchid-600">
+              Communauté
             </a>
           </div>
         </div>
@@ -184,13 +184,13 @@ const solutions = [
     description: 'Le classement des Makers les plus demandé dans le podcast',
     href: '/makers',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" /></svg>'
-  },
-  {
-    name: 'Communauté',
-    description: 'Rejoin la communauté sur discord',
-    href: '/discord',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" /></svg>'
   }
+  // {
+  //   name: 'Communauté',
+  //   description: 'Rejoin la communauté sur discord',
+  //   href: '/discord',
+  //   icon: '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" /></svg>'
+  // }
 ]
 const resources = [
   { name: 'Écouter le podcast', description: 'Choisi ta plateforme préféré où l\'écouter.', href: '/' },
