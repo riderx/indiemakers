e4cb7a02e23c70f2de34df85ee355cfe0ee2ed58
riderx <martindonadieu@gmail.com>
2021-05-13 07:51:35 +0100

    fix: remove analyze

--- nuxt.config.js
+++ nuxt.config.js
@@ -162,7 +162,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    analyze: true,
+    // analyze: true,
   },
   purgeCSS: {
     whitelistPatterns: [/vm--/, /icon--/, /label--/, /vue-dialog/, /vue-modal/]
