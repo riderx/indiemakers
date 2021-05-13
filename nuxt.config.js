bd5ff94c9d8dc9042851c01beec34cdc43f41951
riderx <martindonadieu@gmail.com>
2021-05-13 23:49:11 +0100

    fix: deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -164,7 +164,7 @@ export default {
     // analyze: true,
   },
   purgeCSS: {
-    whitelistPatterns: [/vm--/, /icon--/, /label--/, /vue-dialog/, /vue-modal/]
+    whitelistPatterns: [/vm--/, /icon--/, /label--/, /vue-dialog/, /vue-modal/],
   },
   sentry: {
     dsn: 'https://1e9603c479b54389ab04a4be985e1768@o449238.ingest.sentry.io/5431873', // Enter your project's DSN here
