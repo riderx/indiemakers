7c682e6a43999ddae341787216ebd7dea3fc27b0
riderx <martindonadieu@gmail.com>
2021-05-13 07:57:29 +0100

    fix: remove nuxt axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,7 +110,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    // '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
