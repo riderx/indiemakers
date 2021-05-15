3a3659422c8d39f2921a9a30cf0e5b06ef35b5e6
riderx <martindonadieu@gmail.com>
2021-05-15 20:50:08 +0100

    fix: vercel try again

--- api/bot/index.ts
+++ api/bot/index.ts
@@ -7,29 +7,27 @@ import {
 } from 'discord-interactions'
 import discordInteraction from '../../services/discord/bot'
 import { sendTxtLoading } from '../../services/discord/bot/utils'
-// function getRawBody(req: Request): Promise<string> {
-//   return new Promise((resolve) => {
-//     const bodyChunks: Buffer[] = []
-//     req.on('end', () => {
-//       const rawBody = Buffer.concat(bodyChunks).toString('utf8')
-//       resolve(rawBody)
-//     })
-//     req.on('data', (chunk) => bodyChunks.push(chunk))
-//   })
-// }
+function getRawBody(req: Request): Promise<string> {
+  return new Promise((resolve) => {
+    const bodyChunks: Buffer[] = []
+    req.on('end', () => {
+      const rawBody = Buffer.concat(bodyChunks).toString('utf8')
+      resolve(rawBody)
+    })
+    req.on('data', (chunk) => bodyChunks.push(chunk))
+  })
+}
 
 const bot = async (req: Request, res: Response) => {
-  if (req.method !== 'POST') return res.status(404).end()
-
   try {
-    // const rawBody = await getRawBody(req as any)
+    const rawBody = await getRawBody(req as any)
     // req.body
-    console.error('req.body', JSON.stringify(req.body))
-    // console.error('rawBody', rawBody)
+    console.error('req.body', req.body)
+    console.error('rawBody', rawBody)
     const signature = String(req.headers['X-Signature-Ed25519']) || ''
     const timestamp = String(req.headers['X-Signature-Timestamp']) || ''
     const isValidRequest = await verifyKey(
-      JSON.stringify(req.body),
+      rawBody,
       signature,
       timestamp,
       String(process.env.CLIENT_PUBLIC_KEY)
