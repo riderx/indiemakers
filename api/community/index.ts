203742e962e406542ef88ff75a845d9c94d049be
riderx <martindonadieu@gmail.com>
2021-05-16 04:37:00 +0100

    fix: add api firebase init

--- api/community/index.ts
+++ api/community/index.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express'
+import fFnit from '../../services/firebase_init'
 
 import { getAllUsers } from '../../services/discord/bot/user'
 
 const list = async (_req: Request, res: Response) => {
+  fFnit()
   const data = await getAllUsers()
   data.users.forEach((user) => {
     user.makerlogHook = ''
--- api/maker/index.ts
+++ api/maker/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express'
 import { getAllProjects } from '../../services/discord/bot/project'
 import { getUsersById } from '../../services/discord/bot/user'
+import fFnit from '../../services/firebase_init'
 
 const maker = async (req: Request, res: Response) => {
   if (req?.query?.id) {
+    fFnit()
     const user = await getUsersById(String(req.query.id))
     const projects = await getAllProjects(String(req.query.id))
     if (user) {
