a13cbfc108457fbea8f669bff8ddc471b47b37ff
riderx <martindonadieu@gmail.com>
2021-05-16 04:59:23 +0100

    fix: firebase init

--- services/firebase_init.ts
+++ services/firebase_init.ts
@@ -3,7 +3,12 @@ import admin from 'firebase-admin'
 export default () => {
   if (!admin.apps.length) {
     if (process.env.GOOGLE_SERVICE_ACCOUNT) {
-      admin.initializeApp(JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT))
+      admin.initializeApp({
+        credential: admin.credential.cert(
+          JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT)
+        ),
+        databaseURL: process.env.FIREBASE_DATABASE,
+      })
     } else {
       admin.initializeApp()
     }
