f450778c89e5a0d3c4f3eeb5852a666e0db0981b
martindonadieu <martindonadieu@gmail.com>
2021-09-13 14:36:05 +0300

    chrore: remove old comments

--- api/sitemap/index.ts
+++ api/sitemap/index.ts
@@ -13,10 +13,9 @@ const sitemap = async (_req: Request, res: Response) => {
     const smStream = new SitemapStream({ hostname: 'https://indiemakers.fr' })
 
     smStream.write({ url: '/tools', changefreq: 'monthly', priority: 0.1 })
-    smStream.write({ url: '/makers', changefreq: 'daily', priority: 0.1 })
+    smStream.write({ url: '/makers', changefreq: 'daily', priority: 0.5 })
     smStream.write({ url: '/articles', changefreq: 'daily', priority: 1 })
     const eps = await feed()
-    // const results = await func.run('getMakers')
     let prio = 1.0
 
     eps.forEach((element: any) => {
@@ -54,12 +53,6 @@ const sitemap = async (_req: Request, res: Response) => {
         priority: 0.5,
       })
     })
-    // results.forEach((element) => {
-    //   smStream.write({ url: `/makers/${element.login}`, changefreq: 'daily', priority: 0.1 })
-    // })
-    // Indie Makers Are Taking Back The Web One App At A Time
-    // les indie maker conquerissent le web une app a la fois
-    // les-indie-maker-conquerissent-le-web-une-app-a-la-fois
     smStream.end()
     const data = await streamToPromise(smStream)
     return res.send(data.toString())
