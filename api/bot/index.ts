2803f83ac69b2060387f365e6d14cdf1476d6ca8
martindonadieu <martindonadieu@gmail.com>
2021-10-29 04:06:08 +0100

    fix: use same solution as stripe for rawbody

--- api/bot/index.ts
+++ api/bot/index.ts
@@ -1,26 +1,8 @@
 import { Request, Response } from 'express'
 import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions'
-import getRawBody from 'raw-body'
-import { parse } from 'content-type'
 import fFnit from '../../services/firebase/init'
 import discordInteraction from '../../services/discord/bot'
 import { sendTxtLoading } from '../../services/discord/bot/utils'
-
-const getRaw = (req: Request): Promise<string> =>
-  new Promise((resolve, reject) => {
-    getRawBody(
-      req,
-      {
-        length: req.headers['content-length'],
-        limit: '1mb',
-        encoding: parse(req).parameters.charset,
-      },
-      (err, string) => {
-        if (err) return reject(err)
-        resolve(string)
-      }
-    )
-  })
 
 export const config = {
   api: {
@@ -32,10 +14,9 @@ const bot = async (req: Request, res: Response) => {
   console.error('bot')
   fFnit()
   try {
-    const rawBody: string = await getRaw(req)
     const signature = String(req.headers ? req.headers['x-signature-ed25519'] : req.get('X-Signature-Ed25519'))
     const timestamp = String(req.headers ? req.headers['x-signature-timestamp'] : req.get('X-Signature-Timestamp'))
-    const isValidRequest = await verifyKey(rawBody, signature, timestamp, String(process.env.CLIENT_PUBLIC_KEY))
+    const isValidRequest = await verifyKey((req as any).rawBody, signature, timestamp, String(process.env.CLIENT_PUBLIC_KEY))
     if (!isValidRequest) {
       return res.status(401).end('Bad request signature')
     }
--- services/local.ts
+++ services/local.ts
@@ -1,11 +1,11 @@
 import { readdirSync } from 'fs'
-import express, { Request, Response } from 'express'
+import express, { json, Request, Response, Router } from 'express'
 import * as dotenv from 'dotenv'
 import { lateBot, morningBot } from './discord/bot/schedule'
 
 dotenv.config()
 const app = express()
-const appRouter = express.Router()
+const appRouter = Router()
 
 const morning = async (_req: Request, res: Response) => {
   console.error('bot')
@@ -18,7 +18,15 @@ const late = async (_req: Request, res: Response) => {
   return res.status(200).end('Late send')
 }
 // app.use(express.json())
-app.use((req, res, next) => (req.path.includes('bot') ? next() : express.json()(req, res, next)))
+app.use((req, res, next) =>
+  req.path.includes('bot')
+    ? json({
+        verify(req, _res, buf) {
+          ;(req as any).rawBody = buf.toString()
+        },
+      })(req, res, next)
+    : json()(req, res, next)
+)
 
 readdirSync('./api').forEach((file) => {
   if (!file.startsWith('.')) {
