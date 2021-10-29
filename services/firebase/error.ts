08ad42d51638a61a5125484309119af2ee3da181
martindonadieu <martindonadieu@gmail.com>
2021-10-29 01:57:39 +0100

    feat: add createdAt in error col

--- services/firebase/error.ts
+++ services/firebase/error.ts
@@ -4,7 +4,7 @@ export const sendError = async (err: unknown) => {
   try {
     await firestore()
       .collection('errors')
-      .add(err as any)
+      .add({ ...(err as any), createdAt: new Date().toISOString() })
   } catch (err) {
     console.error(err)
   }
