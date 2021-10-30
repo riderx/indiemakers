acead8c4329f2cf8fed30b3242f11fc100c27c17
martindonadieu <martindonadieu@gmail.com>
2021-10-30 01:29:57 +0100

    fix: disalow rss robots

--- api/robots/index.ts
+++ api/robots/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 const robot = (_req: Request, res: Response) => {
   try {
-    const data = 'User-agent: *\nAllow: /\nSitemap: https://indiemakers.fr/sitemap.xml'
+    const data = 'User-agent: *\nAllow: /\nUser-agent: *\nDisallow: /rss.xml\nSitemap: https://indiemakers.fr/sitemap.xml'
     if (res.type) {
       res.type('text/plain')
     }
