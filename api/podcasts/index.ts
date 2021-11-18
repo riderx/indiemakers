77b119cef5ffb0b4b9f7481a3f4ec3a2dd7125fa
martindonadieu <martindonadieu@gmail.com>
2021-11-18 17:42:48 +0000

    feat: add random ep selection

--- api/podcasts/index.ts
+++ api/podcasts/index.ts
@@ -6,6 +6,9 @@ const feed = async (req: Request, res: Response) => {
   initF()
   if (req?.query?.guid) {
     return res.json(await getOnePodcastById(req.query.guid as string))
+  } else if (req?.query?.random) {
+    const random = (await getAllPodcast()).sort(() => 0.5 - Math.random()).slice(0, Number(req?.query?.random))
+    return res.json(random)
   } else {
     return res.json(await getAllPodcast())
   }
