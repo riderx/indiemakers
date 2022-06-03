1d9f44acb57f33ae53035959e6c614bdde648288
martindonadieu <martindonadieu@gmail.com>
2022-06-03 15:19:49 +0100

    fix: add back missing firebase

---
+++ functions/.eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+module.exports = {
+  root: false,
+  env: {
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: [],
+
+}
---
+++ functions/src/newletter.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios'
+import { config } from 'firebase-functions'
+
+const configSecret = config()
+
+export const sendUserToRevue = (email: string, firstName: string): Promise<AxiosResponse<any>> =>
+  axios({
+    method: 'POST',
+    url: '/api/v2/subscribers',
+    baseURL: 'https://www.getrevue.co',
+    headers: {
+      Authorization: `Token ${configSecret.getrevue.apikey}`,
+    },
+    data: {
+      email,
+      first_name: firstName,
+      double_opt_in: false,
+    },
+  })
