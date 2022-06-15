67179cccb0182ba4c242379aee970b12fa5d7ac6
martindonadieu <martindonadieu@gmail.com>
2022-06-15 17:38:22 +0100

    fix: upgrade to rc4

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 export const createUser = async (email, password) => {
   const auth = getAuth()
@@ -16,36 +16,6 @@ export const signInUser = async (email, password) => {
     const errorMessage = error.message
   })
   return credentials
-}
-
-export const initUser = async () => {
-  const auth = getAuth()
-  const firebaseUser = useFirebaseUser()
-  firebaseUser.value = auth.currentUser
-
-  const userCookie = useCookie('userCookie')
-
-  const router = useRouter()
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-    } else {
-      // if signed out
-      router.push('/')
-    }
-
-    firebaseUser.value = user
-
-    // @ts-ignore
-    userCookie.value = user // ignore error because nuxt will serialize to json
-
-    $fetch('/api/auth', {
-      method: 'POST',
-      body: { user },
-    })
-  })
 }
 
 export const signOutUser = async () => {
